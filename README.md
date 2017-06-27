87b1e1ae7e9a0b70b9b7b536451ef1fb65b465ad
Huey Lee <leejinhuey@gmail.com>
2017-06-27 14:06:25 +0800

    all done

---
+++ README.md
@@ -0,0 +1,13 @@
+# [is-isbn](https://www.npmjs.com/package/is-isbn)
+
+This npm package validates your 10-digit & 13-digit ISBN codes according to its check digits.
+
+    const isIsbn = require('is-isbn')
+    
+    isIsbn.validate("030788743X") // true
+    isIsbn.validate("130788743X") // false
+    isIsbn.validate("0060523794") //true
+    isIsbn.validate("9780670020553") // true
+    isIsbn.validate("9780670020555") // false
+    
+No dependencies.
---
+++ is-isbn.js
@@ -0,0 +1,52 @@
+function isIsbn(){}
+
+isIsbn.prototype.validate = isbn => {
+	isbn = String(isbn)
+	isbn = isbn.split('')
+	if(isbn[9] === "X"){isbn[9] = "10"}
+	if(isbn.length === 10){
+		let validateSum = isbn => {
+			let sum = 0
+			isbn.forEach((digit, index) => {
+				digit = parseInt(digit)
+				sum += digit * (index+1)
+			})
+			return (sum % 11 === 0)
+		}
+		let checkSum = isbn => {
+			let check = parseInt(isbn.pop()), sum = 0
+			isbn = isbn.reverse()
+			isbn.forEach((digit, index) => {
+				sum += parseInt(digit) * (index+2)
+			})
+			sum = (11 - (sum % 11)) % 11
+			return sum === check
+		}
+		return validateSum(isbn) && validateSum(isbn.reverse()) && checkSum(isbn)
+	} else if (isbn.length === 13){
+		let checkSum = isbn => {
+			let check = parseInt(isbn.pop()), sum = 0
+			isbn.forEach((digit, index) => {
+				index++
+				if(index%2 === 0){
+					sum += parseInt(digit)*3
+				} else {
+					sum += parseInt(digit)
+				}
+			})
+			return (10-(sum % 10)) === check
+		}
+		return checkSum(isbn)
+	} else {
+		return false
+	}
+}
+
+module.exports = new isIsbn()
+/*
+let a = new isIsbn()
+let codes = ["030788743X","9780307887436","0670020559","9780670020553","0060523794","9780060523794"]
+codes.forEach(v=>{
+	console.log(v, a.validate(v))
+})
+*/
